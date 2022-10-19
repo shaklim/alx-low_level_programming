d1c2cdb8f39670dffd14cdf35f82eb1aef8f2894
Shakur <shaklim12@outlook.com>
2022-10-18 21:15:04 -0700

    hm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - prints the string "Holberton" from a character array.
+ * main - prints from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "main";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p'); 
+	_putchar('u'); 
+	_putchar('t'); 
+	_putchar('c'); 
+	_putchar('h'); 
+	_putchar('a'); 
+	_putchar('r'); 
+	_putchar('\n'); 
 
 	return (0);
 }
