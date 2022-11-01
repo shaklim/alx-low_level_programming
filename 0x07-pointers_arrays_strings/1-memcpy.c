d6bfedbb5e2d3698b2e093eb41d16c09897c08eb
Shakur <shaklim12@outlook.com>
2022-11-01 15:27:33 -0700

    base

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _memcpy - Copies @n bytes from the memory area pointed
- *           to by @src into that pointed to by @dest.
+ * by @src into that pointed to by @dest
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
  * @n: The number of bytes to copy from @src.
